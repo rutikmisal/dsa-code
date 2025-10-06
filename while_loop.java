5f9709bbaeeb4e9a371c9907fc7b3ba0b758246f
Rutik Misal <rutikmisal2004@gmail.com>
2025-10-06 21:45:58 +0530

    Add files via upload

---
+++ while_loop.java
@@ -0,0 +1,13 @@
+public class while_loop 
+{
+    public static void main(String[] args) 
+    {
+        int count=0;
+        while(count<10)
+        {
+            System.out.println("Hello Rutik........");
+            count++;
+        }
+    }
+    
+}
