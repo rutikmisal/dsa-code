2e67d7a9dc35f06d4b451a1436c7ae418b07b4e1
Rutik Misal <rutikmisal2004@gmail.com>
2025-10-08 22:06:40 +0530

    Add files via upload

---
+++ continue_ky.java
@@ -0,0 +1,13 @@
+public class continue_ky
+{
+    public static void main(String[] args) {
+        for(int i=1;i<=7;i++)
+        {
+            if(i==4)
+            {
+                continue;
+            }
+            System.out.println("i Value "+i);
+        }
+    } 
+}
---
+++ do_while.java
@@ -0,0 +1,19 @@
+public class do_while 
+{
+    public static void main(String[] args) 
+    {
+        int i=0;
+        do
+        {
+            
+            if(i==5)
+            {
+                break;
+            }
+            System.out.println(i);
+            i++;
+        }while(true);
+        
+    }
+    
+}
