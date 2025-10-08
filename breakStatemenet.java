2c0eb7b9b72eb10342d3281301a5ff6c19cb7903
Rutik Misal <rutikmisal2004@gmail.com>
2025-10-08 21:57:55 +0530

    Add files via upload

---
+++ breakStatemenet.java
@@ -0,0 +1,16 @@
+public class breakStatemenet
+{
+    public static void main(String args[])
+    {
+        
+        for(int i=1;i<=5;i++)
+        {
+            if(i==3)
+            {
+                break;
+            }
+            System.out.println("i value "+i);
+        }
+    }
+    
+}
