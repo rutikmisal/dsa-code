6f63bf39cd6ca6895959f93866c0e75de397f3f9
Rutik Misal <rutikmisal2004@gmail.com>
2026-03-22 21:24:22 +0530

    Add files via upload

---
+++ printNum_1to5.java
@@ -0,0 +1,17 @@
+class printNum_1to5
+{
+	public static void main(String args[])
+	{
+		int n=1;
+		printNumber(n); //n=1
+	}
+	public static void printNumber(int n)
+	{
+		if(n==6)
+		{
+			return;
+		}
+		System.out.println(n);
+		printNumber(n+1);
+	}
+}
