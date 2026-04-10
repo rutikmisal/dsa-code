eabf8228633f3183fa270d4a2bea25647f06e35c
Rutik Misal <rutikmisal2004@gmail.com>
2026-04-10 22:07:12 +0530

    Add files via upload

---
+++ Backspace_String_Compare.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Backspace_String_Compare {
+    public boolean backspaceCompare(String s, String t) {
+        return build(s).equals(build(t));
+    }
+
+    public String build(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : str.toCharArray()) {
+            if(ch != '#') {
+                stack.push(ch);
+            } else if(!stack.isEmpty()) {
+                stack.pop();
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for(char ch : stack) {
+            result.append(ch);
+        }
+
+        return result.toString();
+    }
+}
