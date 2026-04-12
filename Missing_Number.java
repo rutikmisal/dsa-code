eb46c882b3e5a48a8602f4b080b428b7e5e9091a
Rutik Misal <rutikmisal2004@gmail.com>
2026-04-12 19:45:34 +0530

    Add files via upload

---
+++ Missing_Number.java
@@ -0,0 +1,19 @@
+class Missing_Number {
+    public int missingNumber(int[] nums) {
+        
+        int n = nums.length;
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num : nums) {
+            set.add(num);
+        }
+
+        for(int i = 0; i <= n; i++) {
+            if(!set.contains(i)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
