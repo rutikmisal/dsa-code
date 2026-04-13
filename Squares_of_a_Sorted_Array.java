4a8f136b6cbedd8692130794d9a761e7eabfc988
Rutik Misal <rutikmisal2004@gmail.com>
2026-04-13 21:53:25 +0530

    Add files via upload

---
+++ Squares_of_a_Sorted_Array.java
@@ -0,0 +1,31 @@
+class Squares_of_a_Sorted_Array {
+    public int[] sortedSquares(int[] nums) {
+        
+        int n = nums.length;
+        int[] result = new int[n];
+        
+        int left = 0;
+        int right = n - 1;
+        int index = n - 1;
+
+        while(left <= right)
+        {
+            int leftSq = nums[left] * nums[left];
+            int rightSq = nums[right] * nums[right];
+
+            if(leftSq > rightSq)
+            {
+                result[index] = leftSq;
+                left++;
+            }
+            else
+            {
+                result[index] = rightSq;
+                right--;
+            }
+            index--;
+        }
+
+        return result;
+    }
+}
