af23ff6bf13ccf758da5c1a85c8ed4d821edbbe4
Rutik Misal <rutikmisal2004@gmail.com>
2026-04-21 21:19:24 +0530

    Add files via upload

---
+++ Sqrt_x.java
@@ -0,0 +1,34 @@
+public class Sqrt_x {
+    public static int mySqrt(int x) 
+    {
+        if (x < 2) return x;
+
+        int left = 1, right = x / 2;
+        int result = 0;
+
+        while (left <= right) 
+        {
+            int mid = left + (right - left) / 2;
+            long square = (long) mid * mid;
+
+            if (square == x) 
+            {
+                return mid;
+            } 
+            else if (square < x) 
+            {
+                result = mid; 
+                left = mid + 1;
+            } 
+            else 
+            {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+    public static void main(String args[])
+    {
+	System.out.println(mySqrt(4));
+    }
+}
